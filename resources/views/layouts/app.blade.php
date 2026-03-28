ede74fd5e410880d2504a27baf5dc02c15715995
YUuSHA07 <yusrizalhakim574@gmail.com>
2026-03-28 22:05:43 +0700

    update(sidebar): update sidebar dosen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
 
                 <div class="my-4 border-t border-indigo-800/50"></div>
 
+                <!-- Laboran -->
                 @if(auth()->user()->role === 'Laboran')
                     <p class="px-4 text-[10px] font-bold text-indigo-400 uppercase tracking-widest mb-2">Laboran</p>
                     <x-nav-link-sidebar :href="route('users.index')" :active="request()->routeIs('users.index')">
@@ -45,15 +46,24 @@
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
                         Buat Kelas
                     </x-nav-link-sidebar>
+
+                <!-- Dosen -->
                 @elseif(auth()->user()->role === 'Dosen')
                     <p class="px-4 text-[10px] font-bold text-indigo-400 uppercase tracking-widest mb-2">Dosen</p>
-                    <x-nav-link-sidebar :href="'#'">Validasi Nilai</x-nav-link-sidebar>
+                    <x-nav-link-sidebar :href="route('courses.index')" :active="request()->routeIs('courses.index')">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+                        Daftar Kelas
+                    </x-nav-link-sidebar>
+
+                <!-- Aslab -->
                 @elseif(auth()->user()->role === 'Aslab')
                     <p class="px-4 text-[10px] font-bold text-indigo-400 uppercase tracking-widest mb-2">Aslab</p>
                     <x-nav-link-sidebar :href="route('courses.index')" :active="request()->routeIs('courses.index')">
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
                         Daftar Kelas
                     </x-nav-link-sidebar>
+
+                <!-- Mahasiswa -->
                 @elseif(auth()->user()->role === 'Mahasiswa')
                     <p class="px-4 text-[10px] font-bold text-indigo-400 uppercase tracking-widest mb-2">Mahasiswa</p>
                     <x-nav-link-sidebar :href="route('courses.index')" :active="request()->routeIs('courses.index')">
