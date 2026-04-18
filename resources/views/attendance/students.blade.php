76c71853296dbfc777883c0519fee03bdc21a466
YUuSHA07 <yusrizalhakim574@gmail.com>
2026-04-18 21:02:18 +0700

    update

--- resources/views/attendance/students.blade.php
+++ resources/views/attendance/students.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header_title">Daftar Peserta: {{ $course->course_name }}</x-slot>
 
-    <div class="max-w-[95rem] mx-auto py-8 px-4">
+    <div class="max-w-[95rem] mx-auto py-0 px-4">
         
         {{-- Alert Notifikasi --}}
         @if(session('success'))
