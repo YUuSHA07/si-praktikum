c76f2310905e96b1b8034eb34697b402bfa19488
YUuSHA07 <yusrizalhakim574@gmail.com>
2026-04-30 00:25:16 +0700

    update(seed)

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,38 +15,12 @@ class UserSeeder extends Seeder
     {
         // 1. Akun Laboran
         User::create([
-            'id' => 'LAB-001',
+            'id' => '123',
             'name' => 'Admin Laboratorium',
             'email' => 'laboran@uinsu.ac.id',
             'password' => Hash::make('password123'),
-            'role' => 'Laboran',
+            'role' => 'laboran',
         ]);
 
-        // 2. Akun Dosen
-        User::create([
-            'id' => 'NIP-19850101', 
-            'name' => 'Dr. Ilka Zufria, M.Kom',
-            'email' => 'ilkazufria@uinsu.ac.id',
-            'password' => Hash::make('password123'),
-            'role' => 'Dosen',
-        ]);
-
-        // 3. Akun Aslab
-        User::create([
-            'id' => 'ASL-2026-01',
-            'name' => 'Asisten Praktikum AI',
-            'email' => 'aslab@uinsu.ac.id',
-            'password' => Hash::make('password123'),
-            'role' => 'Aslab',
-        ]);
-
-        // 4. Akun Mahasiswa
-        User::create([
-            'id' => '0701213000',
-            'name' => 'Mahasiswa Praktikan',
-            'email' => 'mahasiswa@uinsu.ac.id',
-            'password' => Hash::make('password123'),
-            'role' => 'Mahasiswa',
-        ]);
     }
 }
